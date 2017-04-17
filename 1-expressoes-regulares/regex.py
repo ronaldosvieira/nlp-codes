301ae1bf2649346fd2e71786083c860f01bc20de
Ronaldo Vieira <ronaldoesv@gmail.com>
2017-04-17 13:33:31 +0000

    Melhoria na captura de datas

--- 1-expressoes-regulares/regex.py
+++ 1-expressoes-regulares/regex.py
@@ -1,8 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import re
 
 patterns = {
   'num': r'[0-9]+', 
-  'data': r'[0-3]?[0-9]/[0-1]?[0-9]/[0-9]{2,4}',
+  'data': (
+    r'[0-3]?[0-9]' # dia
+    r'(/|\s*de\s*)' # separador 1
+    r'([0-3]?[0-9]|[jJ]aneiro|[fF]evereiro|[mM]arço|[aA]bril|[mM]aio|'
+    r'[jJ]unho|[sS]etembro|[oO]utubro|[nN]ovembro|[dD]ezembro)' # mês
+    r'((/|\s*de\s*)' # separador 2
+    r'[0-9]{2,4})?'), # ano
   'endereco': r'[rR]ua \w+, \d+',
   'telefone': r'(\(\d{2}\))?\s*\d?\s*\d{4}\s*-?\s*\d{4}'
 }
