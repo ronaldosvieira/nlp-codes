c675461750b07304a77218880427ce9400ec8300
Ronaldo Vieira <ronaldoesv@gmail.com>
2017-04-18 02:27:30 +0000

    Alterações no trabalho 1 (commitão)

--- 1-expressoes-regulares/regex.py
+++ 1-expressoes-regulares/regex.py
@@ -1,29 +1,27 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import re
+import re, sys
 
 patterns = {
   'num': r'[0-9]+', 
-  'data': (
-    r'[0-3]?[0-9]' # dia
-    r'(/|\s*de\s*)' # separador 1
-    r'([0-3]?[0-9]|[jJ]aneiro|[fF]evereiro|[mM]arço|[aA]bril|[mM]aio|'
-    r'[jJ]unho|[sS]etembro|[oO]utubro|[nN]ovembro|[dD]ezembro)' # mês
-    r'((/|\s*de\s*)' # separador 2
-    r'[0-9]{2,4})?'), # ano
+  'data': [
+    r'[0-3]?[0-9]/[0-3]?[0-9](/[0-9]{2,4})?',
+    (r'([0-2]?[0-9]\s*de\s*)?([jJ]aneiro|[fF]evereiro|[mM]arço|'
+    r'[aA]bril|[mM]aio|[jJ]unho|[aA]gosto|[sS]etembro|[oO]utubro|'
+    r'[nN]ovembro|[dD]ezembro)(\s*de\s*[0-9]{2,4})?')
+  ],
   'endereco': r'[rR]ua \w+, \d+',
   'telefone': r'(\(\d{2}\))?\s*\d?\s*\d{4}\s*-?\s*\d{4}'
 }
 
-text = raw_input()
-
-while text:
-    text = re.sub(patterns['data'], '_DATA_', text)
-    text = re.sub(patterns['telefone'], '_TELEFONE_', text)
-    text = re.sub(patterns['endereco'], '_ENDERECO_', text)
-    text = re.sub(patterns['num'], '_NUM_', text)
-    
-    print(text)
-    
-    text = raw_input()
+for line in sys.stdin:
+  line = line.rstrip("\r\n")
+  
+  line = re.sub(patterns['data'][0], '_DATA_', line)
+  line = re.sub(patterns['data'][1], '_DATA_', line)
+  line = re.sub(patterns['telefone'], '_TELEFONE_', line)
+  line = re.sub(patterns['endereco'], '_ENDERECO_', line)
+  line = re.sub(patterns['num'], '_NUM_', line)
+  
+  print(line)
