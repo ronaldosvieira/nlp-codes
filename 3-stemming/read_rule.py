d012f49bda18ce0402555687bb5c280eb919bde3
Ronaldo Vieira <ronaldoesv@gmail.com>
2017-04-18 19:04:02 +0000

    Adição da função de leitura das regras

--- 3-stemming/read_rule.py
+++ 3-stemming/read_rule.py
@@ -1,8 +1,25 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-def plural_reduction(word):
-    pass
+def rule(path="rules/step0.pt"):
+    file = open(path, "r")
+    lines = file.readlines()
+    
+    rules = dict()
+    
+    for i in range(1, len(lines)):
+        tokens = lines[i].replace("\"", "").split("\t")
+        
+        exceptions = list()
+        
+        if "*" not in tokens[3]:
+            exceptions = tokens[3].split(",")
+
+        rules[tokens[0]] = (tokens[2], tokens[1], exceptions)
+        print(lines[i])
+        print(rules[tokens[0]])
+    
+    return rules
     
 if __name__ == '__main__':
-	init()
+	rule()
