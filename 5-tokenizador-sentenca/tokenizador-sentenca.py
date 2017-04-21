af24859364ed1e75d4cf860866b5a9aee39d0cf4
Ronaldo Vieira <ronaldoesv@gmail.com>
2017-04-21 17:34:04 +0000

    Implementação da tokenização por sentenças

---
+++ 5-tokenizador-sentenca/tokenizador-sentenca.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+import re
+import unicodedata
+
+def sentence_tokenize(text):
+    tokens = list()
+    
+    pre_tokens = re.split(r"([\.\;\?\!\:]+)", text)
+    
+    for p in range(0, len(pre_tokens) - 2, 2):
+        tokens.append( pre_tokens[p] +  pre_tokens[p+1] )
+    
+    return tokens
+
+def remove_accents(word):
+    return "".join((c for c in unicodedata.normalize('NFD', word) if unicodedata.category(c) != 'Mn'))
+
+def main():
+    text = ''.join([re.sub(r"[(\r+)(\n+)(\t+)]", " ", line) for line in sys.stdin])
+    text = remove_accents(text)
+    
+    print(' ->  ' + '\n -> '.join(sentence_tokenize(text)))
+        
+if __name__ == '__main__':
+    main()
