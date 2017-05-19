17b7d0c22b454132378f158a63cf70956a18a992
Geovani Celebrim <geovanicelebrim@gmail.com>
2017-05-19 20:34:06 +0000

    Adição do dataset de spams em sms

--- 7-naive-bayes/naive-bayes.py
+++ 7-naive-bayes/naive-bayes.py
@@ -2,18 +2,36 @@
 # -*- coding: utf-8 -*-
 
 import sys
+from random import shuffle
 from documents import Document, Model
 
 def main():
     model = Model()
+    docs = list()
     
     for line in sys.stdin:
         doc = line.rstrip("\r\n").strip().split(" ")
         cl = doc.pop(0)
         
-        model.add_doc(Document(cl, doc))
+        docs.append(Document(cl, doc))
     
-    print(model.classify("Chinese Chinese Chinese Tokyo Japan"))
+    shuffle(docs)
+    
+    train = docs[:int(len(docs) * .8)]
+    test = docs[len(train):]
+    
+    [model.add_doc(doc) for doc in train]
+    
+    results = list()
+    
+    for doc in test:
+        result = model.classify(" ".join(doc.get_words()))
+        
+        results.append((max(result, key=result.get), doc.get_class()))
+
+    precision = sum(1 for res in results if res[0] == res[1])
+    
+    print("Precision: ", precision / len(results))
 
 if __name__ == '__main__':
 	main()
